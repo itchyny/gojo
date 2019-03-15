07e8a2d5719ba6c6b9aa08392ca59d248ef4c858
itchyny <itchyny@hatena.ne.jp>
2019-03-15 22:17:27 +0900

    add tests for gojo cli

---
+++ cli/cli_test.go
@@ -0,0 +1,135 @@
+package cli
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCliRun(t *testing.T) {
+	testCases := []struct {
+		name     string
+		args     []string
+		input    string
+		expected string
+		err      string
+	}{
+		{
+			name: "default",
+			args: []string{"foo=bar"},
+			expected: `{"foo":"bar"}
+`,
+		},
+		{
+			name: "multiple",
+			args: []string{"foo=bar", "bar=baz", "qux=quux"},
+			expected: `{"foo":"bar","bar":"baz","qux":"quux"}
+`,
+		},
+		{
+			name: "pretty",
+			args: []string{"-p", "foo=bar", "bar=baz", "qux=quux"},
+			expected: `{
+  "foo": "bar",
+  "bar": "baz",
+  "qux": "quux"
+}
+`,
+		},
+		{
+			name: "nested object",
+			args: []string{"-p", `foo={"bar":{"foo":1,"baz":"qux","quux":["foo"]}}`},
+			expected: `{
+  "foo": {
+    "bar": {
+      "foo": 1,
+      "baz": "qux",
+      "quux": [
+        "foo"
+      ]
+    }
+  }
+}
+`,
+		},
+		{
+			name: "array",
+			args: []string{"-a", "foo", "bar", "baz", "false", "0x40"},
+			expected: `["foo","bar","baz",false,64]
+`,
+		},
+		{
+			name: "array pretty",
+			args: []string{"-a", "-p", "foo", "0xf0", `{"foo":{"bar":30}}`},
+			expected: `[
+  "foo",
+  240,
+  {
+    "foo": {
+      "bar": 30
+    }
+  }
+]
+`,
+		},
+		{
+			name: "hyphen hyphen",
+			args: []string{"-a", "--", "-p", "foo"},
+			expected: `["-p","foo"]
+`,
+		},
+		{
+			name: "input",
+			args: []string{"-a", "-p"},
+			input: `foo
+{"bar":100}
+`,
+			expected: `[
+  "foo",
+  {
+    "bar": 100
+  }
+]
+`,
+		},
+		{
+			name: "input error",
+			input: `foo
+bar
+`,
+			err: `failed to parse: "foo"`,
+		},
+		{
+			name: "parse error",
+			args: []string{"foo"},
+			err:  `failed to parse: "foo"`,
+		},
+		{
+			name: "unkown flag",
+			args: []string{"-b"},
+			err:  `flag provided but not defined: -b`,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			outStream := new(bytes.Buffer)
+			errStream := new(bytes.Buffer)
+			cli := cli{
+				inStream:  strings.NewReader(tc.input),
+				outStream: outStream,
+				errStream: errStream,
+			}
+			err := cli.run(tc.args)
+			if tc.err == "" {
+				require.NoError(t, err)
+				assert.Equal(t, tc.expected, outStream.String())
+			} else {
+				require.Error(t, err)
+				assert.Equal(t, tc.err, err.Error())
+			}
+		})
+	}
+}
